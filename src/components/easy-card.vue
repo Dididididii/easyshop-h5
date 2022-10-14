841377d6401602102af7e4889c88f78f72099675
Dididididii <1281627297@qq.com>
2022-10-14 16:05:27 +0800

    首页商品卡片进行封装组件

---
+++ src/components/easy-card.vue
@@ -0,0 +1,65 @@
+<template>
+  <div :style="{display:'flex', flexWrap: warp?'wrap':'nowrap',justifyContent: 'space-around'}">
+    <div class="goodsCard" v-for="i in 6" :key="i" @click="toGoods">
+        <van-image
+            class="goodsImage"
+            radius="8px"
+            fit="cover"
+            lazy-load
+            src="https://yanxuan-item.nosdn.127.net/ddb4a80ac97a175bc633f0a53076815a.jpg"
+        />
+        <p class="goodsTitle">儿童多色圆领印花短袖T恤110-160cm</p>
+        <p class="goodsPirce">￥59.00</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'easy-Card',
+    props:{
+        warp:{
+            type:Boolean,
+            default:true
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+.goodsCard:last-child {
+        margin-right: 1;
+}
+.goodsCard{
+        width: 160px;
+        height: 233px;
+        text-align: center;
+        margin: 5px;
+        border-radius: 8px;
+        background-color: #fff;
+        padding-bottom: 5px;
+        .goodsImage{
+          width: 160px;
+          height: 160px;
+        }
+        .goodsTitle {
+          width: 135px;
+          height: 40px;
+          overflow: hidden;
+          text-overflow:ellipsis;
+          font-size: 15px;
+          padding: 0px 10px;
+          // text-indent: 10px;
+          margin-right: 5px;
+        }
+        p{
+          margin: 0;
+        }
+        .goodsPirce{
+          font-size: 18px;
+          margin-top: 5px;
+          color:red;
+          font-weight: 500;
+        }
+      }
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -54,49 +54,19 @@
                 <span>新鲜出炉 品质靠谱</span>
               </div>
               <div class="goodsRecommend">
-                <div class="goodsCard" v-for="i in 6" :key="i" @click="toGoods">
-                  <van-image
-                    class="goodsImage"
-                    radius="8px"
-                    fit="cover"
-                    lazy-load
-                    src="https://yanxuan-item.nosdn.127.net/ddb4a80ac97a175bc633f0a53076815a.jpg"
-                  />
-                  <p class="goodsTitle">儿童多色圆领印花短袖T恤110-160cm</p>
-                  <p class="goodsPirce">￥59.00</p>
-                </div>
+                <easyCard :warp="false" />
               </div>
               <!-- 猜你喜欢 -->
               <div class="title">
                 <h3>猜你喜欢</h3>
               </div>
               <div class="goodsLike">
-                <div class="goodsCard" v-for="i in 6" :key="i" @click="toGoods">
-                  <van-image
-                    class="goodsImage"
-                    radius="8px"
-                    fit="cover"
-                    lazy-load
-                    src="https://yanxuan-item.nosdn.127.net/ddb4a80ac97a175bc633f0a53076815a.jpg"
-                  />
-                  <p class="goodsTitle">儿童多色圆领印花短袖T恤110-160cm</p>
-                  <p class="goodsPirce">￥59.00</p>
-                </div>
+                <easyCard />
               </div>
             </main>
             <main v-else>
               <div class="goodsLike">
-                <div class="goodsCard" v-for="i in 6" :key="i" @click="toGoods">
-                  <van-image
-                    class="goodsImage"
-                    radius="8px"
-                    fit="cover"
-                    lazy-load
-                    src="https://yanxuan-item.nosdn.127.net/ddb4a80ac97a175bc633f0a53076815a.jpg"
-                  />
-                  <p class="goodsTitle">儿童多色圆领印花短袖T恤110-160cm</p>
-                  <p class="goodsPirce">￥59.00</p>
-                </div>
+                <easyCard />
               </div>
             </main>
           </van-tab>
@@ -106,8 +76,10 @@
   </template>
   
   <script>
+  import easyCard from '@/components/easy-card.vue'
   export default {
       name:'easy-Home',
+      components:{easyCard},
       data(){
           return {
               isToken:false,
@@ -212,41 +184,8 @@
           background:none;
       }
     }
-    .goodsCard{
-        width: 160px;
-        height: 233px;
-        text-align: center;
-        margin: 5px;
-        border-radius: 8px;
-        background-color: #fff;
-        padding-bottom: 5px;
-        .goodsImage{
-          width: 160px;
-          height: 160px;
-        }
-        .goodsTitle {
-          width: 135px;
-          height: 40px;
-          overflow: hidden;
-          text-overflow:ellipsis;
-          font-size: 15px;
-          padding: 0px 10px;
-          // text-indent: 10px;
-          margin-right: 5px;
-        }
-        p{
-          margin: 0;
-        }
-        .goodsPirce{
-          font-size: 18px;
-          margin-top: 5px;
-          color:red;
-          font-weight: 500;
-        }
-      }
-      .goodsCard:last-child {
-        margin-right: 1;
-      }
+    
+      
     .goodsLike{
       margin-left: 5px;
       display: flex;
