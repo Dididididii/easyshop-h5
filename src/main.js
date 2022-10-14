566def4e96d1dbf8f4de1983703caf8cb1b5a852
Dididididii <1281627297@qq.com>
2022-10-14 10:11:44 +0800

    设置页面静态啊我那次

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import 'amfe-flexible'
-import { Search,Tab, Tabs,Swipe, SwipeItem,Lazyload,Image as VanImage,Tabbar, TabbarItem,Icon,Sidebar, SidebarItem,NavBar,SwipeCell,Card,Button,SubmitBar,Checkbox,Empty,Popup  } from 'vant'
+import { Search,Tab, Tabs,Swipe, SwipeItem,Lazyload,Image as VanImage,Tabbar, TabbarItem,Icon,Sidebar, SidebarItem,NavBar,SwipeCell,Card,Button,SubmitBar,Checkbox,Empty,Popup,Cell, CellGroup,Field,AddressList,Toast,AddressEdit  } from 'vant'
 
 Vue.config.productionTip = false
 
@@ -27,6 +27,12 @@ Vue.use(SubmitBar)
 Vue.use(Checkbox)
 Vue.use(Empty)
 Vue.use(Popup)
+Vue.use(Cell)
+Vue.use(CellGroup)
+Vue.use(Field)
+Vue.use(AddressList)
+Vue.use(Toast)
+Vue.use(AddressEdit)
 
 new Vue({
   router,
--- src/views/contact/index.vue
+++ src/views/contact/index.vue
@@ -66,10 +66,76 @@
         </div>
       </div>
       <van-button  type="default" block size="small">退出登录</van-button>
-      <van-popup v-model="updateShow" position="right" :style="{ height: '100%', width:'100%' }" closeable >
-        <div class="updateBox">
-          123123
+      <van-popup v-model="updateShow" position="right" :style="{ height: '100%', width:'100%' }" >
+        <van-nav-bar
+          title="标题"
+          left-arrow
+          @click-left="updateShow=false"
+        />
+        <div class="updateBox" >
+           <!-- 修改用户名的 -->
+          <div class="upateUser" v-if="false">
+            <div class="photoBox">
+            <van-image
+            class="photo"
+            fit="cover"
+            lazy-load
+            round
+            src="https://img01.yzcdn.cn/vant/cat.jpeg"
+            />
+          </div>
+          <van-cell is-link title="名字" @click="show = true" />
+          <van-popup v-model="show" position="right" :style="{ height: '100%', width:'100%' }"  >
+            <div>
+              <van-nav-bar
+                title="标题"
+                left-arrow
+                @click-left="show=false"
+              />
+              <div>
+                <van-cell-group>
+                  <van-field v-model="value" placeholder="请输入用户名" />
+                  <van-button type="default" block size="small">修改</van-button>
+                </van-cell-group>
+              </div>
+            </div>
+          </van-popup>
+          </div>
+          <!-- 查看收货地址 -->
+          <div class="updateAddress" >
+            <van-address-list
+              v-model="chosenAddressId"
+              :list="list"
+              default-tag-text="默认"
+              @add="onAdd"
+              @edit="onEdit"
+            />
+            <van-popup v-model="addressShow" position="right" :style="{ height: '100%', width:'100%' }"  >
+            <div>
+              <van-nav-bar
+                title="标题"
+                left-arrow
+                @click-left="addressShow=false"
+              />
+              <div>
+                <van-address-edit
+                  :area-list="areaList"
+                  show-postal
+                  show-delete
+                  show-set-default
+                  show-search-result
+                  :search-result="searchResult"
+                  :area-columns-placeholder="['请选择', '请选择', '请选择']"
+                  @save="onSave"
+                  @delete="onDelete"
+                  @change-detail="onChangeDetail"
+                />
+              </div>
+            </div>
+          </van-popup>
+          </div>
         </div>
+        
       </van-popup>
     </van-popup>
   </div>
@@ -115,13 +181,74 @@ export default {
           }
         ],
         setShow:false,
-        updateShow:false
+        updateShow:false,
+        show:false,
+        value:'',
+        chosenAddressId: '1',
+        addressShow:false,
+        areaList:{
+          110000: '北京市',
+          120000: '天津市',
+        },
+        searchResult: [],
+        list: [
+          {
+            id: '1',
+            name: '张三',
+            tel: '13000000000',
+            address: '浙江省杭州市西湖区文三路 138 号东方通信大厦 7 楼 501 室',
+            isDefault: true,
+          },
+          {
+            id: '2',
+            name: '李四',
+            tel: '1310000000',
+            address: '浙江省杭州市拱墅区莫干山路 50 号',
+          },
+        ]
       }
+    },
+    methods:{
+      onAdd() {
+        this.$toast('新增地址');
+      },
+      onEdit() {
+        this.addressShow=true
+        // this.$toast('编辑地址:' + index);
+      },
+      onSave() {
+        this.$toast('save');
+      },
+      onDelete() {
+        this.$toast('delete');
+      },
+      onChangeDetail(val) {
+        if (val) {
+          this.searchResult = [
+            {
+              name: '黄龙万科中心',
+              address: '杭州市西湖区',
+            },
+          ];
+        } else {
+          this.searchResult = [];
+        }
+      },
     }
 }
 </script>
 
 <style lang="less" scoped>
+.photoBox{
+  display: flex;
+  margin: 10px 0;
+  .photo{
+    width:64px;
+    height: 64px;
+    margin: auto;
+  }
+}
+
 .right {
   font-size:14px;
 }
@@ -135,7 +262,7 @@ export default {
 
 }
 
-.setBox ,.updateBox{
+.setBox {
   margin:50px 10px 0  10px;
 }
 .userCard{
