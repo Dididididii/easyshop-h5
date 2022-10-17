d7e51f38451597355743ef8f7aec8a6ef72dc975
Dididididii <1281627297@qq.com>
2022-10-17 16:28:53 +0800

    商品详情商品规格选择功能完成

--- src/views/goods/index.vue
+++ src/views/goods/index.vue
@@ -10,8 +10,8 @@
     </van-nav-bar>
     <main>
         <!-- 商品轮播图 -->
-        <van-swipe @change="onChange">
-            <van-swipe-item v-for="item in goods.mainPictures" :key="item">
+        <van-swipe @change="onChange" >
+            <van-swipe-item v-for="(item , index) in goods.mainPictures" :key="item" @click="openImage(goods.mainPictures , index)">
                 <van-image
                     class="goodsSwiperImage"
                     fit="scale-down"
@@ -33,7 +33,7 @@
         <!-- 商品规格 -->
         <div class="goodsCard">
             <div class="skuItem">
-                    <van-cell title="选择规格" is-link >
+                    <van-cell title="选择规格" is-link @click="show=true" >
                         <template #label>
                             <div class="box">
                                 <van-image
@@ -99,11 +99,19 @@
             <van-goods-action-button type="danger" text="立即购买" />
         </van-goods-action>
     </footer>
+    <van-sku
+        v-model="show"
+        :sku="sku"
+        :goods="goodsList"
+        :goods-id="$route.query.id"
+        :hide-stock="sku.hide_stock"
+    />
   </div>
 </template>
 
 <script>
 import { getGoods } from '@/api/goods'
+import { ImagePreview } from 'vant';
 import dayjs from 'dayjs'
 var relativeTime = require('dayjs/plugin/relativeTime')
 import * as isLeapYear from 'dayjs/plugin/isLeapYear' // 导入插件
@@ -115,16 +123,103 @@ export default {
     data(){
         return {
             current: 0,
-            goods:[]
+            goods:[],
+            show:false,
+            sku: {
+                // 所有sku规格类目与其值的从属关系，比如商品有颜色和尺码两大类规格，颜色下面又有红色和蓝色两个规格值。
+                // 可以理解为一个商品可以有多个规格类目，一个规格类目下可以有多个规格值。
+                tree: [],
+                // 所有 sku 的组合列表，比如红色、M 码为一个 sku 组合，红色、S 码为另一个组合
+                list: [],
+                price: '100.00', // 默认价格（单位元）
+                stock_num: 227, // 商品总库存
+                collection_id: 2261, // 无规格商品 skuId 取 collection_id，否则取所选 sku 组合对应的 id
+                none_sku: false, // 是否无规格商品
+                hide_stock: false // 是否隐藏剩余库存
+            },
+            goodsList: {
+                // 默认商品 sku 缩略图
+                picture: ''
+            },
+            
         }
     },
     methods: {
+        openImage(images , index) {
+            ImagePreview({
+            images,
+            startPosition: index,
+            });
+        },
         onChange(index) {
             this.current = index;
         },
         async getGood() {
+            let sku = []
+            let lists = []
             const res = await getGoods(this.$route.query.id)
             this.goods = res.result
+            this.sku.price = res.result.price
+            this.sku.stock_num = res.result.inventory
+            this.goodsList.picture = res.result.mainPictures[0]
+            res.result.specs.forEach((item,index) =>{
+                sku.push(
+                    {
+                        k:item.name,
+                        k_s:`s${index+1}`,
+                        v:[]
+                    }
+                )
+                item.values.forEach((skus,i) => {
+                    if(skus.picture) {
+                        sku[0].v.push(
+                            {
+                                id:i+1,
+                                name:skus.name,
+                                imgUrl:skus.picture,
+                                previewImgUrl:skus.picture
+                            }
+                        )
+                    } else {
+                        sku[1].v.push(
+                            {
+                                id:i+1,
+                                name:skus.name,
+                            }
+                        )
+                    }
+                })
+            })
+            this.sku.tree = sku
+            res.result.skus.forEach((item) =>{
+                lists.push(
+                    {
+                        id:item.id,
+                        price:item.price*100,
+                        s1:'',
+                        s2:'',
+                        stock_num:item.inventory,
+                        specs:item.specs
+                    }
+                )
+            })
+            
+            lists.forEach((list,index) => {
+                sku.forEach(item => {
+                    item.v.forEach( v =>{
+                        list.specs.forEach(specs => {
+                            if(specs.valueName === v.name) {
+                                if(v.imgUrl){
+                                    lists[index].s1=v.id
+                                } else {
+                                    lists[index].s2=v.id
+                                }
+                            }
+                        })
+                    })
+                })
+            })
+            this.sku.list = lists
         },
         day(day) {
             dayjs.extend(relativeTime)
